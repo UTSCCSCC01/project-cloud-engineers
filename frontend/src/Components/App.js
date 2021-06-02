3bb0c7bbc651ffc1d89e51aeb02278606bd70080
Saad <m.saad.raza1999@gmail.com>
2021-06-02 08:40:06 -0400

    adding react router for client side routing

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,23 +1,23 @@
+import { Switch, Route, Link } from 'react-router-dom'
+
 import logo from '../logo.svg';
 import '../Styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/'>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p> Edit <code>src/App.js</code> and save to reload.</p>
+            
+            <Link className="App-link" to={`/register`}>Register</Link>
+
+          </header>
+        </Route>
+        
+      </Switch>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './Components/App';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
