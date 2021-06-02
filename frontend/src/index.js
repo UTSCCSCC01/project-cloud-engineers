1e60b2d82ab8753be4201721e7e8daef6f6dc9ca
Saad <m.saad.raza1999@gmail.com>
2021-06-02 09:38:05 -0400

    added fireabse context to allow firebase to be used anywhere in the component tree

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from './Components/App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './Components/Utils/Auth';
+import { FirebaseProvider } from './Components/Utils/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <Router>
-        <App />
-      </Router>
-    </AuthProvider>
+    <FirebaseProvider>
+      <AuthProvider>
+        <Router>
+          <App />
+        </Router>
+      </AuthProvider>
+    </FirebaseProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
