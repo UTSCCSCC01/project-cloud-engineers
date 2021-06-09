7d79b5e08c2f7b815e943ff650e7d9ae77ebfff0
DoctorTinku <anissaha86@gmail.com>
2021-06-09 01:17:28 -0400

    CLOUD-39 : Form Frontend + Backend
    - converted frrontend component to form
    - database fields are rendered and updated dynamically
    - backend complete

--- frontend/src/Components/UserPreferences.js
+++ frontend/src/Components/UserPreferences.js
@@ -3,95 +3,136 @@ import { useHistory } from "react-router-dom";
 import '../Styles/App.css';
 
 import { useAuth } from './Utils/Auth';
-import { Component, useState } from "react";
+import { useFirebase } from './Utils/Firebase';
+import { useState } from "react";
+
+import { useCollectionData } from "react-firebase-hooks/firestore"
 
 function UserPreferences() {
-  let auth = useAuth();
-  let history = useHistory();
+  const auth = useAuth();
+
+  const firebase = useFirebase();
+  const db = firebase.firestore();
+  const DB_USER_LOCATION = 'users';
+
+  // Fetching user data from database
+  const uid = JSON.parse(localStorage.getItem('user')).userID;
+  const [user, loading] = useCollectionData(db.collection(DB_USER_LOCATION).where('uid', '==', uid));
+  console.log(uid);
+
+  // Specifying which fields are editable
+  const editableFields = ['username', 'firstname', 'lastname', 'bio', 'school'];
+  const fieldLabels = ["Username", "First Name", "Last Name", "Bio", 'School'];
   
+
+  // Allows for toggling between rendering profile view mode and profile edit mode
+  const [renderViewState, setRenderViewState] = useState(true);
+
+  // Array state for saving text within form inputs
+  const [inputs, setInputs] = useState(editableFields.map(() => ""));
+
+  // Updates the ith input state 
+  const setInputHelper = (event, i) => {
+      const inputsCopy = inputs;
+      inputsCopy[i] = event.target.value;
+      setInputs(inputsCopy);
+  };
+
+  const handleSaveChanges = (event) => {
+
+    // Prevents page refresh upon form submission
+    event.preventDefault();
+
+    // Creates JSON of updated data
+    const updatedUserData = Array.from(editableFields.keys()).reduce((obj, i) => 
+        ({...obj, [editableFields[i]]: inputs[i] }), {});
+    console.log(updatedUserData);
+
+    // Updating Firebase
+    db.collection(DB_USER_LOCATION).doc(uid).update(updatedUserData)
+    .then(
+        (val) => {
+            console.log('Profile Backend Updated!', val);
+        },
+        (err) => {
+            console.log('Could not update profile :(', err);
+        }
+    );
+
+    setRenderViewState(true);
+  };
+
+  const handleEdit = () => {
+      setRenderViewState(false);
+      const newInputs = editableFields.map((f) => user[0][f] ? user[0][f] : "");
+      setInputs(newInputs);
+  };
+  
+  const handleCancel = () => {
+    console.log(inputs);
+    setRenderViewState(true);
+  };
+
+  // Helper render function for edit state of page
+  const renderEditMode = () => {
+      return ( 
+        <form onSubmit={handleSaveChanges}>
+
+            {Array.from(fieldLabels.keys()).map((i) => <p><b>{fieldLabels[i]}: </b> 
+            <input defaultValue={
+
+                user[0][editableFields[i]] ? user[0][editableFields[i]] : ""
+                
+            }
+            
+            onChange={(e) => setInputHelper(e, i)}/></p>)}
+
+            <button type='submit' onClick={handleSaveChanges}>Save Changes</button>
+            <button onClick={handleCancel}>Cancel</button>
+        </form>);
+  };
+
+
+  //Helper render funciton for view state of page
+  const renderViewMode = () => {
+      
+    if (loading) {
+        return <p>loading...</p>
+    }
+
+    console.log(user)
+    
+    return (
+        <div>
+            {
+                Array.from(editableFields.keys()).map((i) => 
+                    <p><b>{fieldLabels[i]}: </b> 
+                    {
+    
+                        user[0][editableFields[i]] ? user[0][editableFields[i]] : "No Data"
+                        
+                    }</p>)
+            }
+
+            <button onClick={handleEdit}>Edit</button>
+
+        </div>
+      )
+  }
+
+  //Render for component 
   return (
-    <div className="App">
-        <header className="App-header">
-            <Link class="App-link" to={'/home'}>Back to Home</Link>
-            <h2>Update Fields here</h2>
-            <UserField fieldName={"First Name"} value={"Anis"}/>
-            <UserField fieldName={"Last Name"} value={"Saha"}/>
-            <UserField fieldName={"Username"} value={"anissaha86"}/>
-        </header>
+    <div>
+        <Link className="App-link" to={'/home'}>Back to Home</Link>
+
+        <h1>Update Profile</h1>
+        <h2>Current Data</h2>
+
+        {renderViewState ? renderViewMode() : renderEditMode()}
+
+       
     </div>
   );
 }
 
 export default UserPreferences;
-
-/**
- *         <div>
-            <profileField fieldname="Username" getField={() => 'Anis Saha'}/> 
-            <profileField fieldname="Bio" getField={() => 'Likes Space'}/> 
-        </div>
- */
-
-class UserField extends Component {
-    state = {
-        editMode: false,
-        value: this.props.value,
-        input: ''
-    };
-
-    handleEdit = () => {
-        console.log('entering edit mode');
-        this.setState({ editMode: true,
-                        input: this.state.value});
-        this.render();
-    };
-
-    handleSaveChanges = () => {
-        this.setState({ value: this.state.input, 
-                        editMode: false,
-                        input: '' });
-        this.render();
-    };
-
-    handleCancel = () => {
-        this.setState({ editMode: false, 
-                        input: '' });
-        this.render();
-    };
-
-    handleInputChange = (event) =>
-        this.state.input = event.target.value
-
-    renderViewMode() {
-        return (
-            <div>
-                <div>
-                    <h4>{this.props.fieldName}</h4>
-                    <h3>{this.state.value}</h3>
-                </div>
-                <button onClick={this.handleEdit}>Edit</button>
-            </div>
-        );
-    };
-
-    renderEditMode() { 
-        return (
-            <div>
-                <div>
-                    <h4>{this.props.fieldName}</h4>
-                    <input defaultValue={ this.state.input } onChange={(event) => this.handleInputChange(event)} />
-                </div>
-                <button onClick={this.handleSaveChanges}>Save Changes</button>
-                <button onClick={this.handleCancel}>Cancel</button>
-            </div>
-            
-        );
-    }
-
-    render() {
-        if (this.state.editMode) {
-            return this.renderEditMode();
-        }
-        
-        return this.renderViewMode();
-    };
-}
