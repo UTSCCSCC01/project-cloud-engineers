95506817d492a36d2f5ac341a59baccafa33a669
Saad <m.saad.raza1999@gmail.com>
2021-07-01 17:58:09 -0400

    CLOUD-61 got all module items from DB, now need to enable selection

--- frontend/src/Components/ELearning/CoursePages/CreateModule.js
+++ frontend/src/Components/ELearning/CoursePages/CreateModule.js
@@ -1,17 +1,44 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useParams } from 'react-router-dom';
 import { nanoid } from 'nanoid';
+import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
+
 import { useFirebase } from '../../Utils/Firebase';
 
 
 function CreateAssignment(props) {
     let { courseId } = useParams();
     let user = JSON.parse(localStorage.getItem("user"));
-    let [formData, setFormData] = useState({ title: '', description: '' });
+    
+    let [formData, setFormData] = useState({ title: ''});
+    let [modItems, setModItems] = useState([]);
+    let [allItems, setAllItems] = useState([]);
 
     let firebase = useFirebase();
     let db = firebase.firestore();
+    const [lessons, lessLoading, lessError] = useCollectionDataOnce(db.collection("lessons").where('courseId', '==', courseId));
+    const [assigns, assLoading, assError] = useCollectionDataOnce(db.collection("assignments").where('courseId', '==', courseId));
+    
 
+    useEffect(() => {
+        if (lessons && assigns) {
+            console.log("here")
+            let list = [];
+            assigns.forEach((item) => {
+                list.push({
+                    ...item,
+                    type: 'assignment'
+                })
+            })
+            lessons.forEach((item) => {
+                list.push({
+                    ...item,
+                    type: 'lesson'
+                })
+            })
+            setAllItems(old => list);
+        }
+    }, [lessons, assigns])
 
     let handleChange = (e) => {
         setFormData((old) => {
@@ -25,24 +52,22 @@ function CreateAssignment(props) {
     let handleSubmit =  async (e) => {
         e.preventDefault();
         let modId = nanoid();
-        await firebase.firestore().collection('modules').doc(modId).set({
-            ...formData,
-            moduleId: modId,
-            courseId: courseId,
-            creatorId: user.userID,
-            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        });
-        
-        setFormData({ title: '', description: '' });
+        // await firebase.firestore().collection('modules').doc(modId).set({
+        //     ...formData,
+        //     moduleId: modId,
+        //     courseId: courseId,
+        //     creatorId: user.userID,
+        //     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        // });
+        setFormData({ title: ''});
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" name="title" value={formData.title} onChange={handleChange} placeholder="title" required />
-                <input type="text" name="description" value={formData.description} onChange={handleChange} placeholder="descrip" required />
+                <p>{JSON.stringify(allItems)}</p>
                 
-                <br />
                 
 
                 <input type="submit" value="Create Module" />
