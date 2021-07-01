83e9b01f18fcf536df4ea14a8f7c63b991bc8ff7
razamu15 <47922655+razamu15@users.noreply.github.com>
2021-07-01 12:19:07 -0400

    Feature course creation (#28)
    * CLOUD-18 added component for course creation
    
    * CLOUD-18 completed course creation form and added instrcutor condition in elearning home page
    
    * CLOUD-18 added warning if a user is not enrolled in any courses

--- frontend/src/Components/ELearning/ELearning.js
+++ frontend/src/Components/ELearning/ELearning.js
@@ -4,6 +4,7 @@ import { Switch, Route, useRouteMatch, Link } from 'react-router-dom';
 
 import { useFirebase } from '../Utils/Firebase';
 import Course from './Course';
+import CreateCourse from './CreateCourse';
 import Card from '../Utils/Card';
 
 function Elearning() {
@@ -13,35 +14,44 @@ function Elearning() {
   let firebase = useFirebase();
   let db = firebase.firestore();
 
-  const [data, loading, error] = useCollectionDataOnce(db.collection("courses").where('students', 'array-contains', `${user.userID}`));
+  const [data, loading, error] = useCollectionDataOnce(
+    user.role === 'instructor' ? db.collection("courses") :
+      db.collection("courses").where('students', 'array-contains', `${user.userID}`)
+  );
 
   return (
     <Switch>
       <Route exact path={path}>
         <div className="home__page">
-          <Link to={`${path}/testurlid`}>Its a me</Link>
+          {user.role === 'instructor' ? <Link to={`${path}/create-course`}>creates new course</Link> : <></>}
 
           {loading ? <p>Loading...</p> : (
             error ? <p>{console.log(error)}Error...</p> :
-              <div className="selectionPage">
-                { data.map(course => {
-                    return (
-                      <Card
-                        imgSrc="https://www.elegantthemes.com/blog/wp-content/uploads/2020/06/Divi-Community-Update-May-2020-scaled.jpg"
-                        key={course.Id}
-                        title={course.title}
-                        description={course.description}
-                        linkPath={`${path}/${course.courseId}`}
-                        buttonText="Go to course"
-                      />
-                    )
-                  })
+              <div>
+                {data.length < 1 ? <h1>You are not enrolled in any courses</h1> :
+                  <div className="selectionPage">
+                    {data.map(course => {
+                      return (
+                        <Card
+                          imgSrc="https://www.elegantthemes.com/blog/wp-content/uploads/2020/06/Divi-Community-Update-May-2020-scaled.jpg"
+                          key={course.courseId}
+                          title={course.title}
+                          description={course.description}
+                          linkPath={`${path}/${course.courseId}`}
+                          buttonText="Go to course"
+                        />
+                      )
+                    })
+                    }
+                  </div>
                 }
               </div>
-            )}
+          )}
         </div>
       </Route>
-
+      <Route path={`${path}/create-course`}>
+        <CreateCourse />
+      </Route>
       <Route path={`${path}/:courseId`}>
         <Course />
       </Route>
