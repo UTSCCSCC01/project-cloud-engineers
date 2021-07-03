ec7cb4ba7e650b75cc7d131a7332848a5f2a247e
Mustafa-Hafeez <mustafahafeez.99@gmail.com>
2021-07-02 20:45:45 -0400

    CLOUD-17 adding in elearning cnavigation and access

--- frontend/src/Components/ELearning/Course.js
+++ frontend/src/Components/ELearning/Course.js
@@ -5,20 +5,58 @@ import { Switch, Route, useRouteMatch, Link, useParams, useLocation } from 'reac
 
 import Modules from './CoursePages/Modules';
 import Assignments from './CoursePages/Assignments';
+import People from './CoursePages/People';
 import CreateAssignment from './CoursePages/CreateAssignment';
 import CreateLesson from './CoursePages/CreateLesson';
 import CreateModule from './CoursePages/CreateModule';
 
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+
 function Course(props) {
+    //https://material-ui.com/components/drawers/ Template for drawer component
     let { courseId } = useParams();
     let { path } = useRouteMatch();
+    const drawerWidth = 240;
+
     let user = JSON.parse(localStorage.getItem("user"));
     
-
     let firebase = useFirebase();
     let db = firebase.firestore();
-    const [values, loading, error] = useCollectionDataOnce(db.collection("modules").where('courseId', '==', courseId));
+    const [values, loading, error] = useCollectionDataOnce(db.collection("courses").where('courseId', '==', courseId));
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+        },
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: drawerWidth,
+        },
+        drawerContainer: {
+            overflow: 'auto',
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+        },
+        }));
+
+    const classes = useStyles();
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
@@ -27,12 +65,51 @@ function Course(props) {
         <Switch>
             <Route exact path={path}>
                 <div>
-                <Link to={`${courseId}/modules`}>Modules</Link>
-                <br />
-                <Link to={`${courseId}/assignments`}>Assignments</Link>
+                    <div className={classes.root}>
+                        <CssBaseline />
+                        <AppBar position="fixed" className={classes.appBar}>
+                            <Toolbar>
+                            <Typography variant="h6" noWrap>
+                                E-Learning
+                            </Typography>
+                            </Toolbar>
+                        </AppBar>
+                        <Drawer
+                            className={classes.drawer}
+                            variant="permanent"
+                            classes={{
+                            paper: classes.drawerPaper,
+                            }}
+                        >
+                            <Toolbar />
+                            <div className={classes.drawerContainer}>
+                            <List>
+                                <ListItem button>
+                                    <Link className="App-link" to={'/home/e-learning'}>Back to E-Learning</Link>
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <List>
+                                <ListItem button>
+                                    <Link to={`${courseId}/modules`}>Modules</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <Link to={`${courseId}/assignments`}>Assignments</Link>
+                                </ListItem>
+                                <ListItem button>
+                                    <Link to={`${courseId}/people`}>People</Link>
+                                </ListItem>
+                            </List>
+                            </div>
+                        </Drawer>
+                        <main className={classes.content}>
+                            <Toolbar />
+                            <Typography variant="h3">Welcome to {values[0].title}</Typography>
+                            <br></br>
+                            <Typography variant="h6">{values[0].description}</Typography>
 
-                    <h2>course id from url params {courseId}</h2>
-                    <p>we will show title nad description og this course here</p>
+                        </main>
+                    </div>
 
                     { user.role === 'instructor' ? 
                     <div>
@@ -41,14 +118,15 @@ function Course(props) {
                         <Link to={`${courseId}/create-lesson`}>Create New Lesson</Link>
                         <br />
                         <Link to={`${courseId}/create-module`}>Create Module</Link>
-                    </div> : 
-                    <p>you are not instructor so you only see the options above</p>
+                    </div> : true
                     }
-
                 </div>
             </Route>
             <Route path={`${path}/modules`}>
                 <Modules />
+            </Route>
+            <Route path={`${path}/people`}>
+                <People />
             </Route>
             <Route path={`${path}/assignments`}>
                 <Assignments />
--- frontend/src/Components/ELearning/CoursePages/Assignments.js
+++ frontend/src/Components/ELearning/CoursePages/Assignments.js
@@ -2,31 +2,90 @@ import React, { useState, useEffect } from "react";
 import { useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 import { useParams, Link } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Alert from '@material-ui/lab/Alert';
 import { useFirebase } from '../../Utils/Firebase';
 
+
+//Rows styling template: https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/templates/dashboard/Orders.js
 
 function Assignments(props) {
     let { courseId } = useParams();
     let user = JSON.parse(localStorage.getItem("user"));
-
+    let [curAss, setAss] = useState([]);
+    const useStyles = makeStyles((theme) => ({
+        seeMore: {
+          marginTop: theme.spacing(3),
+        },
+      }));
     let firebase = useFirebase();
     let db = firebase.firestore();
-    const [values, loading, error] = useCollectionDataOnce(db.collection("modules").where('courseId', '==', courseId));
+    const [values, loading, error] = useCollectionDataOnce(db.collection("assignments").where('courseId', '==', courseId));
 
+
+    useEffect(()=>{
+        if(values){
+            values.forEach((assignment)=>{
+                let newAssignment= {};
+                newAssignment.title = assignment.title;
+                let date = new Date(assignment.duedate.seconds * 1000);
+
+                let dueDate = (date.getDate()+
+                  "/"+(date.getMonth()+1)+
+                  "/"+date.getFullYear());
+                newAssignment.duedate = dueDate;
+
+
+                setAss(oldArray => [...oldArray, newAssignment]);
+            })
+        }
+    },[values]);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
 
+
+    function generateMarkup(markupArray){
+        return(
+            <React.Fragment>
+            <Alert severity="info">Assignment Information!</Alert>
+
+            <Table size="small">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Due Date</TableCell>
+                  <TableCell>Assignment</TableCell>
+                  <TableCell>Grade</TableCell>
+                  <TableCell align="right">Weight</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {markupArray.map((ass) => (
+                  <TableRow key={ass.assignmentId}>
+                    <TableCell>{ass.duedate}</TableCell>
+                    <TableCell>{ass.title}</TableCell>
+                    <TableCell>N/A</TableCell>
+                    <TableCell align="right">N/A</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </React.Fragment>
+        )
+    }
     return (
         <div>
             {user.role === 'instructor' ?
                 <div>
                     <p> You are instructor: you can also go back and add a new assignment</p>
-                </div> :
-                <p>you are not instructor so you only see the assignment list</p>
+                </div> :  generateMarkup(curAss)
             }
-
-            <h2>shows all the assignments in this course</h2>
+            <Link className="App-link" to={'/home/e-learning/'+courseId}>Back to Course Homepage</Link>
         </div>
     )
 }
