8ba256b12436cffcb6613c580621625f50f624c8
Smit Patel <smitp505@gmail.com>
2021-07-14 00:38:36 -0400

    CLOUD-76: Fixed the file upload for post images (#53)

--- frontend/src/Components/Community/Posts/CommentList.js
+++ frontend/src/Components/Community/Posts/CommentList.js
@@ -25,7 +25,6 @@ function CommentList({postID}) {
         setcontent(e.target.value);
         setnewID(nanoid());
     }
-
     //Gets all comments attached to a specific post from the database.
     useEffect(() => {
         const tempArray = [];
@@ -35,6 +34,17 @@ function CommentList({postID}) {
             querySnapshot.forEach((doc) => {
                 tempArray.push(doc.data())
             });
+            tempArray.sort( (a,b) => {
+                if (a.timestamp < b.timestamp) {
+                    return -1;
+                }
+                else if (a.timestamp == b.timestamp) {
+                    return 0;
+                }
+                else {
+                    return 1;
+                }
+            })
             setcomments(tempArray);
         })
         .catch((error) => {
@@ -46,7 +56,9 @@ function CommentList({postID}) {
     //comments under pages.
     function addComment(event){
         event.preventDefault();
-        
+        if (content === '') {
+            return;
+        }
         const temp = {
             content: content,
             authorId: user.userID,
