546592a4bc11f7c05b142c599dbf06ae27def825
khanm253 <65485711+khanm253@users.noreply.github.com>
2021-07-13 20:18:41 -0400

    CLOUD-90: Users who want to join a company can now be accepted or rejected by user. (#49)
    Co-authored-by: Abdullah Khan <abdullahkhan@Abdullahs-MacBook-Air.local>

---
+++ frontend/src/Components/Community/CompanyProfiles/RequestList.js
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react'
+import { useFirebase } from "../../Utils/Firebase";
+import Request from './Request';
+import '../../../Styles/RequestList.css'
+
+function RequestList() {
+    
+    const firebase = useFirebase();
+    const db = firebase.firestore();
+    let user = JSON.parse(localStorage.user);
+
+    const [requests, setRequests] = useState([]);
+
+    const handleDecision = (requestId) => {
+        let temp = requests.filter((req) => req.reqId != requestId);
+        setRequests(temp);
+    }
+
+    useEffect(() => {
+        async function fetchingData(){
+            await db.collection('requests').where("creatorId", "==", user.userID).get().then((querySnapshot)=>{
+                let temp = []
+                querySnapshot.forEach((doc) =>{
+                    temp.push(doc.data())
+                });
+                setRequests(temp);
+            })
+        }
+        fetchingData();
+    }, [])
+
+    return (
+        <div className="reqlist">
+            <h2>Requests:</h2>
+            {requests.map((req) => (
+                <Request
+                    reqInfo = {req}
+                    handleDecision = {handleDecision}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default RequestList
